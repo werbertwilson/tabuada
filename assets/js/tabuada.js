c41af4759fcea4bf1aebdc1e3928185866cd86c9
Werbert Wilson S. C. da Conceição <werbert.wil@hotmail.com>
2021-10-16 11:36:59 -0300

    js atualizado
    Atualização no JS e no corpo da tabela para selecionar um valor

--- assets/js/tabuada.js
+++ assets/js/tabuada.js
@@ -1,25 +1,35 @@
-let tabuada = document.querySelector("#tabuada tbody");
+function calculaTabuada(){
 
-let valorA = 99;
-
-tabuada.innerHTML = '';
-
-for (let valorB = 0; valorB <=10; valorB++){
-
-    let resultado = valorA * valorB;
-
-    let template = `
-                        <td>${valorA}</td>
-                        <td>x</td>
-                        <td>${valorB}</td>
-                        <td>=</td>
-                        <td>${resultado}</td>
-                
-    `;
-
-    let tr = document.createElement('tr');
-
-    tr.innerHTML = template;
-
-    tabuada.append(tr);
-}
+    let tabuada = document.querySelector("#tabuada tbody");
+    
+    let valorA = parseFloat(document.querySelector("#valorA").value);
+    
+    tabuada.innerHTML = '';
+    
+    for (let valorB = 0; valorB <=10; valorB++){
+    
+        let resultado = valorA * valorB;
+    
+        let template = `
+                            <td>${valorA}</td>
+                            <td>x</td>
+                            <td>${valorB}</td>
+                            <td>=</td>
+                            <td>${resultado}</td>
+                    
+        `;
+    
+        let tr = document.createElement('tr');
+    
+        tr.innerHTML = template;
+    
+        tabuada.append(tr);
+    }
+    
+    
+    }
+    
+    calculaTabuada();
+    
+    document.querySelector("#valorA").addEventListener('change', calculaTabuada);
+    
